303364d39fcb06fa6ee565d49e647823127fd581
Djeno <djeno00@mail.ru>
2024-11-27 13:28:16 +0300

    some fix

--- Lab3/main.py
+++ Lab3/main.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from colorama import Fore, Style, init
 from tabulate import tabulate
 
 
